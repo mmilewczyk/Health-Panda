29512a563f7407f644cb3e391f5b1c4b10d4cf9d
agiklo <54153910+agiklo@users.noreply.github.com>
2020-06-04 21:07:00 +0200

    delete option in workout.html

--- src/main/java/com/agiklo/HeathProject/controler/TrainingController.java
+++ src/main/java/com/agiklo/HeathProject/controler/TrainingController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
@@ -37,8 +38,9 @@ public class TrainingController {
         return "workout";
     }
 
-    @RequestMapping(path = "/workout", method = RequestMethod.DELETE)
-    public void delete(@RequestParam Long id){
+    @RequestMapping(value = "/workout/delete")
+    public String delete(@RequestParam Long id){
         trainingRepository.deleteById(id);
+        return "redirect:/workout";
     }
 }
