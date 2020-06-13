b7f7ea72fb163b2a9f85708e114661c749b8b0c9
agiklo <54153910+agiklo@users.noreply.github.com>
2020-06-13 22:10:34 +0200

    cleaning code in bmr

--- src/main/java/com/agiklo/HeathProject/service/BMRService.java
+++ src/main/java/com/agiklo/HeathProject/service/BMRService.java
@@ -6,35 +6,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class BMRService {
 
+    public double calculate(BMR bmr){
+         return 66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge());
+    }
     //1,0 – leżący lub siedzący tryb życia, brak aktywności fizycznej
     public double bmrCalculator(BMR bmr){
-            double bmrValue = 66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge());
-            return bmrValue;
+        return calculate(bmr);
     }
 
     //1,2 – praca siedząca, aktywność fizyczna na niskim poziomie
     public double bmrCalculator12(BMR bmr){
-        double bmrValue12 = 1.2 * (66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge()));
-        return bmrValue12;
+        return 1.2 * calculate(bmr);
     }
     //1,4 – praca nie fizyczna, trening 2 razy w tygodniu
     public double bmrCalculator14(BMR bmr){
-        double bmrValue14 = 1.4 * (66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge()));
-        return bmrValue14;
+        return 1.4 * calculate(bmr);
     }
     //1,6 – lekka praca fizyczna, trening 3-4 razy w tygodniu
     public double bmrCalculator16(BMR bmr){
-        double bmrValue16 = 1.6 * (66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge()));
-        return bmrValue16;
+        return 1.6 * calculate(bmr);
     }
     //1,8 – praca fizyczna, trening 5 razy w tygodniu
     public double bmrCalculator18(BMR bmr){
-        double bmrValue18 = 1.8 * (66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge()));
-        return bmrValue18;
+        return 1.8 * calculate(bmr);
     }
     //2,0 – ciężka praca fizyczna, codzienny trening
     public double bmrCalculator20(BMR bmr){
-        double bmrValue20 = 2.0 * (66 + (13.7 * bmr.getBodyWeight()) + (5 * bmr.getHeight()) - (6.76 * bmr.getAge()));
-        return bmrValue20;
+        return 2.0 * calculate(bmr);
     }
 }
